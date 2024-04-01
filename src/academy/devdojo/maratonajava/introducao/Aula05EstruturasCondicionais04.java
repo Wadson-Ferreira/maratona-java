54b035430cbbc62ebbcccd17f1075e5b6153bfa8
Wadson Rocha <dev.wadson@gmail.com>
2024-04-01 18:53:52 -0300

    24 - Estruturas Condicionais pt 05 - Resolução do exercícios

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -1,4 +1,5 @@
 package academy.devdojo.maratonajava.introducao;
+import java.util.Scanner;
 /** Dado um determinado salário, qual o valor de imposto tenho que pagar?
  De 0 até 34.712 tem que pagar 9.70%
  De 34.713 até 68.507 tem que pagar 37.35%
@@ -6,14 +7,17 @@ package academy.devdojo.maratonajava.introducao;
  */
 public class Aula05EstruturasCondicionais04 {
     public static void main(String[] args) {
-        double salarioAnual = 70000;
+
         double primeiraFaixa = 9.7 / 100;
         double segundaFaixa = 37.35 / 100;
         double terceiraFaixa = 49.50 / 100;
         double resultado;
         String valorDoImposto = "Você vai pagar um imposto de: ";
-
         String taxa;
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite seu salário anual: ");
+        double salarioAnual = entrada.nextDouble();
 
         if (salarioAnual < 34713){
             taxa = ("Sua taxa é de 9.70%");
