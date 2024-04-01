746ad31899015b3f25e385ebef5669ecf6d06162
Wadson Rocha <dev.wadson@gmail.com>
2024-04-01 15:57:41 -0300

    Estruturas Condicionais pt 02 - ELSE IF

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,12 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
         int idade = 16;
         boolean isAutorizadoComprarBebida = idade >= 18;
         // !
         if(isAutorizadoComprarBebida){
             System.out.println("Autorizado a comprar bebida alcólica");
+        }else{
+            System.out.println("Não autorizado a comprar bebida alcólica");
         }
 
         if(!isAutorizadoComprarBebida){
@@ -15,6 +17,10 @@ public class Aula05EstruturasCondicionais {
 
         if(isAutorizadoComprarBebida == false){
            System.out.println("Não autorizado a comprar bebida alcólica");
+        }
+        boolean c = false;
+        if(c == true){
+            System.out.println("Dentro de algo que nunca deve ser feito");
         }
 
         System.out.println("Fora do if");
