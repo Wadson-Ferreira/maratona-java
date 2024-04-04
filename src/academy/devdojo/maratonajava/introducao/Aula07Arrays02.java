1aa154539102016214829c9527cf14b30dc14667
Wadson Rocha <dev.wadson@gmail.com>
2024-04-04 10:04:26 -0300

    32 - Arrays pt 03 - Impressão de todos os arrays, .length e instanciar novamente o array.

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -6,10 +6,21 @@ public class Aula07Arrays02 {
         //char '\u0000' ' '
         //boolean false
         //String null
-        String[] nomes = new String[3];
-        System.out.println(nomes[0]);
-        System.out.println(nomes[1]);
-        System.out.println(nomes[2]);
+        String[] nomes = new String[4];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
+        nomes[3] = "Hinata";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+
+        nomes = new String[6];
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
 
     }
 }
