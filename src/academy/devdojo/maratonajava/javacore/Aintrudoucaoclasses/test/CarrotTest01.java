3d8157b0c4da78dbf3721dc6637fbd99dad30487
Wadson Rocha <dev.wadson@gmail.com>
2024-05-21 15:35:31 -0300

    42 - Orientação Objetos - Exercício Classes

---
+++ src/academy/devdojo/maratonajava/javacore/Aintrudoucaoclasses/test/CarrotTest01.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.javacore.Aintrudoucaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintrudoucaoclasses.dominio.Carro;
+
+public class CarrotTest01 {
+    public static void main(String[] args) {
+        Carro carro01 = new Carro();
+        Carro carro02 = new Carro();
+
+        carro01.nome = "Fusca Bala";
+        carro01.modelo = "Sport";
+        carro01.ano = 1969;
+
+        carro02.nome = "Mustang";
+        carro02.modelo = "GT 500";
+        carro02.ano = 1968;
+
+        System.out.println("Carro 1");
+        System.out.println(carro01.nome);
+        System.out.println(carro01.modelo);
+        System.out.println(carro01.ano);
+
+        System.out.println("\nCarro 2");
+        System.out.println(carro02.nome);
+        System.out.println(carro02.modelo);
+        System.out.println(carro02.ano);
+    }
+}
