221606a9027bc60472820207a3ed94c12658529f
Wadson <dev.wadson@gmail.com>
2024-10-16 17:18:24 -0300

    106 - Classes Utilitárias - Wrappers pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 1.0F;
+        double doubleP = 1.0D;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 1.0F;
+        Double doubleW = 1.0D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+        //É importante sempre darmos prioridade para trabalhar com tipos
+        // primitvos, utilizando assim as classes wrapper somente quando
+        // necessário por exemplo quando vamos trabalhar com situações onde é
+        // necessário trabalhar com a referencia do objeto.
+    }
+}
