c7e4c38523bf128cced497653fba75129cecc74c
Wadson <dev.wadson@gmail.com>
2024-10-16 17:37:45 -0300

    107 - Classes Utilitárias - Wrappers pt 02

--- src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -13,7 +13,7 @@ public class WrapperTest01 {
 
         Byte byteW = 1;
         Short shortW = 1;
-        Integer intW = 1;
+        Integer intW = 1; //autoboxing
         Long longW = 10L;
         Float floatW = 1.0F;
         Double doubleW = 1.0D;
@@ -24,5 +24,23 @@ public class WrapperTest01 {
         // primitvos, utilizando assim as classes wrapper somente quando
         // necessário por exemplo quando vamos trabalhar com situações onde é
         // necessário trabalhar com a referencia do objeto.
+
+        int i = intW; //unboxing
+        Integer intW2 = Integer.parseInt("1");
+        boolean verdadeiro = Boolean.parseBoolean("TruE");
+        System.out.println(verdadeiro);
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('9'));
+        System.out.println(Character.isLowerCase('A'));
+        System.out.println(Character.isUpperCase('b'));
+        System.out.println(Character.toUpperCase('b'));
+        System.out.println(Character.toLowerCase('B'));
+
+        //Autoboxing é quando você tem um tipo primitivo e o java faz a
+        // conversão automatica para um wrapper e Unboxing é quando você
+        // converte um tipo wrapper para um tipo primitivo.
+
     }
 }
