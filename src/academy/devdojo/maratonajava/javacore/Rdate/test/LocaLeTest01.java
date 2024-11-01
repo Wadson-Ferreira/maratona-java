cca32f6b388d2a3c331b9f0a849391a6ade0e442
Wadson <dev.wadson@gmail.com>
2024-10-31 21:17:13 -0300

    115 - Classes Utilitárias - Internacionalização Datas com Locale

---
+++ src/academy/devdojo/maratonajava/javacore/Rdate/test/LocaLeTest01.java
@@ -0,0 +1,40 @@
+package academy.devdojo.maratonajava.javacore.Rdate.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaLeTest01 {
+    public static void main(String[] args) {
+        /*
+        Locale localeItaly = new Locale("it", "IT");
+        Locale localeCH = new Locale("it", "CH");
+        Locale localeBR = new Locale("pt", "BR");
+        depreciado
+        */
+
+
+        Locale localeItaly = Locale.ITALY;
+        Locale localeCH = Locale.of("ch", "CH");
+        Locale localeBR = Locale.of("pt", "BR");
+
+
+        Calendar calendar = Calendar.getInstance();
+
+        DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL,
+                localeItaly);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL,
+                localeCH);
+
+        DateFormat df3 = DateFormat.getDateInstance(DateFormat.FULL,localeBR);
+        System.out.println("Italia " + df1.format (calendar.getTime()));
+        System.out.println("Suiça " + df2.format(calendar.getTime()));
+        System.out.println("Brasil " + df3.format(calendar.getTime()));
+
+        System.out.println(localeItaly.getDisplayCountry());
+        System.out.println(localeBR.getDisplayCountry(localeItaly));
+        System.out.println(localeCH.getDisplayCountry());
+
+
+    }
+}
