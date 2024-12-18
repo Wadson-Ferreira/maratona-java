eabefaa11f6b3e2e45f0622780108dc6ec1836c9
Wadson <dev.wadson@gmail.com>
2024-12-18 16:45:23 -0300

    141 - Classes Utilitárias - IO pt 04 - BufferedWriter

--- src/academy/devdojo/maratonajava/javacore/VIo/test/FileReaderTest01.java
+++ src/academy/devdojo/maratonajava/javacore/VIo/test/FileReaderTest01.java
@@ -9,15 +9,17 @@ public class FileReaderTest01 {
         File file = new File("file.txt");
 
         try (FileReader fr = new FileReader(file)){
-//            char[] in = new char[30];
-//            fr.read(in);
-//            for (char c : in) {
-//                System.out.print(c);
+//           char[] in = new char[52];
+//           int size = fr.read(in);
+//            System.out.println(size);
+//           for (char c : in) {
+//               System.out.print(c);
 //            }
-            int i;
+           int i;
             while ((i = fr.read()) != -1) {
                 System.out.print((char) i);
             }
+
 
         } catch (IOException e) {
             e.printStackTrace();
