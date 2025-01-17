3b1b5ac285c030b391b7b16d79e60598e766515b
Wadson <dev.wadson@gmail.com>
2025-01-17 15:52:40 -0300

    167 - Coleções pt 07 - List pt 02

--- src/academy/devdojo/maratonajava/javacore/YColecoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/YColecoes/test/ListTest01.java
@@ -5,18 +5,35 @@ import java.util.List;
 
 public class ListTest01 {
     public static void main(String[] args) {
-        List<String> nomes = new ArrayList(16);
+        List<String> nomes = new ArrayList<>(16);
+        List<String> nomes2 = new ArrayList<>(16);
         nomes.add("Genesys");
         nomes.add("William");
+        nomes2.add("Rute");
+        nomes2.add("Lessa");
+
+        nomes.addAll(nomes2);
 
         for (String nome : nomes) {
             System.out.println(nome);
         }
-        nomes.add("Ferrreira");
+
         System.out.println("--------------");
 
-        for (int i = 0; i < nomes.size(); i++) {
+        int size = nomes.size();
+        for (int i = 0; i < size; i++) {
+            nomes.add("Rocha");
             System.out.println(nomes.get(i));
         }
+        nomes.remove(1);
+        nomes.remove("Rocha");
+        System.out.println(nomes);
+
+        //Não se pode criar coleções com tipos primitivos. Deve-se utilizar
+        // os Wrappers.
+        List<Integer> numeros = new ArrayList<>(16);
+        numeros.add(1);
+
+        System.out.println(numeros);
     }
 }
