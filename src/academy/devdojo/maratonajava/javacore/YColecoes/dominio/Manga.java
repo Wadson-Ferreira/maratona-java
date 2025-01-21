735cc5445fa0a36b8b1c06ffa71a418e7a8957e1
Wadson <dev.wadson@gmail.com>
2025-01-21 15:57:43 -0300

    175 - Coleções pt 15 - Set, HashSet

--- src/academy/devdojo/maratonajava/javacore/YColecoes/dominio/Manga.java
+++ src/academy/devdojo/maratonajava/javacore/YColecoes/dominio/Manga.java
@@ -23,15 +23,14 @@ public class Manga  implements Comparable<Manga>{
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Manga manga = (Manga) o;
-        return Double.compare(preco, manga.preco) == 0 && Objects.equals(id, manga.id) && Objects.equals(titulo, manga.titulo);
+        return Objects.equals(id, manga.id) && Objects.equals(titulo, manga.titulo);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, titulo, preco);
+        return Objects.hash(id, titulo);
     }
 
     @Override
