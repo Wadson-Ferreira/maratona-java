ab4ff820fa4c84a47da1ceab6c11482e233f018f
Wadson <dev.wadson@gmail.com>
2025-01-25 13:13:13 -0300

    194 - Parametrizando comportamentos pt 02

---
+++ src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/interfaces/CarroPredicado.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.ZZBcomportamento.interfaces;
+
+import academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio.Carro;
+
+public interface CarroPredicado {
+    boolean test(Carro carro);
+}
