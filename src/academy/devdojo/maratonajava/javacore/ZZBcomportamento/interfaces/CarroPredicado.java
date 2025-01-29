9df46b141250ee6981fb2134102b7cfbcd050749
Wadson <dev.wadson@gmail.com>
2025-01-29 10:00:04 -0300

    195 - Lambdas pt 01 - Predicate

--- src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/interfaces/CarroPredicado.java
+++ src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/interfaces/CarroPredicado.java
@@ -3,5 +3,8 @@ package academy.devdojo.maratonajava.javacore.ZZBcomportamento.interfaces;
 import academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio.Carro;
 
 public interface CarroPredicado {
+    //anonimas, funções, conciso
     boolean test(Carro carro);
+    //(parametro) -> <expressão>
+    //(Car car) -> car.getColor().equals("green")
 }
