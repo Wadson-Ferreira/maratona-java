ed03b7b7ec878bbb0682f711f79c07f2218112d6
Wadson <dev.wadson@gmail.com>
2025-01-31 16:27:59 -0300

    205 - Streams pt 03 - Introduction pt 03

--- src/academy/devdojo/maratonajava/javacore/ZZEstreams/dominio/LightNovel.java
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/dominio/LightNovel.java
@@ -1,5 +1,7 @@
 package academy.devdojo.maratonajava.javacore.ZZEstreams.dominio;
 
+import java.util.Objects;
+
 public class LightNovel {
     private String titulo;
     private double preco;
@@ -15,6 +17,18 @@ public class LightNovel {
                 "titulo='" + titulo + '\'' +
                 ", preco=" + preco +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        LightNovel that = (LightNovel) o;
+        return Objects.equals(titulo, that.titulo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(titulo);
     }
 
     public String getTitulo() {
