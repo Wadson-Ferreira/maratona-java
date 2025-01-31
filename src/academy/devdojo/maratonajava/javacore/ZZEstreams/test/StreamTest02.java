2233a43a7680126917e850bd1176fdc5d9b53cf8
Wadson <dev.wadson@gmail.com>
2025-01-31 16:07:59 -0300

    204 - Streams pt 02 - Introduction pt 02

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest02.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamTest02 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 3.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no Life", 2.99),
+            new LightNovel("Fullmetal Alchemist", 5.99),
+            new LightNovel("Kumo Desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+
+    public static void main(String[] args) {
+        List<String> titulos = lightNovels.stream()
+                .sorted(Comparator.comparing(LightNovel::getTitulo))
+                .filter(ln -> ln.getPreco() <= 4)
+                .limit(3)
+                .map(LightNovel::getTitulo)
+                .collect(Collectors.toList());
+
+        System.out.println(titulos);
+    }
+}
