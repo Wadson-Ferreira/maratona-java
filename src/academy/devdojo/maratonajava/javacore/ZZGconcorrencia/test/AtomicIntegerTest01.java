2e35f7dd7044f994f432c599727d16d523e95204
Wadson <dev.wadson@gmail.com>
2025-02-12 15:33:56 -0300

    230 - Concorrência pt 02 - Lock and ReentrantLock

--- src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/AtomicIntegerTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/AtomicIntegerTest01.java
@@ -1,25 +1,33 @@
 package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Contador {
     private int contador;
+    private final AtomicInteger atomicInteger = new AtomicInteger();
+    private final Lock lock = new ReentrantLock();
 
     public int getContador() {
         return contador;
     }
 
-    private AtomicInteger atomicInteger = new AtomicInteger();
-
     void incrementar() {
-        contador++;
-        atomicInteger.incrementAndGet();
+        lock.lock();
+        try{
+            contador++;
+            atomicInteger.incrementAndGet();
+        }finally {
+            lock.unlock();
+        }
     }
 
     public AtomicInteger getAtomicInteger() {
         return atomicInteger;
     }
 }
+
 public class AtomicIntegerTest01 {
     public static void main(String[] args) throws InterruptedException {
         Contador contador = new Contador();
---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ReentrantLockTEst01.java
@@ -0,0 +1,46 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Trabalhador implements Runnable {
+    private String nome;
+    private ReentrantLock lock;
+
+    public Trabalhador(String nome, ReentrantLock lock) {
+        this.nome = nome;
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        lock.lock();
+        try {
+            if(lock.isHeldByCurrentThread()){
+                System.out.printf("Thread %s entrou em uma sessão crítica%n",nome);
+                System.out.printf("Thread %s vai esperar 2%n",nome);
+                Thread.sleep(2000);
+                System.out.printf("Thread %s finalizou a espera 2%n",nome);
+            }
+            System.out.printf("%d Threads esperando na fila%n", lock.getQueueLength());
+        } catch (InterruptedException e) {
+           e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+public class ReentrantLockTEst01 {
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock();
+        new Thread(new Trabalhador("A", lock)).start();
+        new Thread(new Trabalhador("B", lock)).start();
+        new Thread(new Trabalhador("C", lock)).start();
+        new Thread(new Trabalhador("D", lock)).start();
+        new Thread(new Trabalhador("E", lock)).start();
+        new Thread(new Trabalhador("F", lock)).start();
+        new Thread(new Trabalhador("G", lock)).start();
+
+
+    }
+}
