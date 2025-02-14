2a86c5e44a6e752f5c5327251f51ccdf9fed1914
Wadson <dev.wadson@gmail.com>
2025-02-14 11:31:14 -0300

    237 - Concorrência pt 09 - Executors pt 02 - ScheduledExecutorService

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ScheduledThreadTest01.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledThreadTest01 {
+    private static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+
+    private static final DateTimeFormatter formater = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    private static void beeper(){
+        Runnable r = () -> {
+            System.out.println(LocalTime.now().format(formater) + " beep");
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        //executor.schedule(r,2, TimeUnit.SECONDS);
+
+        //ScheduledFuture<?> scheduleWithFixedDelay = executor.scheduleWithFixedDelay(r, 1, 2, TimeUnit.SECONDS);
+         ScheduledFuture<?> scheduleWithFixedDelay = executor.scheduleAtFixedRate(r,1,2,TimeUnit.SECONDS);
+        executor.schedule(() -> {
+            System.out.println("Desligando o scheduleWithFixedDelay");
+            scheduleWithFixedDelay.cancel(false);
+            executor.shutdown();
+        }, 10, TimeUnit.SECONDS);
+    }
+    public static void main(String[] args) {
+        System.out.println(LocalTime.now().format(formater));
+        beeper();
+    }
+}
