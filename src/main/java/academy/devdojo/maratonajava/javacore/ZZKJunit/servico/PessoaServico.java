5d41992a6c5b41491538931f6baeb8801b51cb7b
Wadson <dev.wadson@gmail.com>
2025-03-18 22:39:40 -0300

    281 - Testes unitários com jUnit pt 02

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZKJunit/servico/PessoaServico.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZKJunit/servico/PessoaServico.java
@@ -2,11 +2,17 @@ package academy.devdojo.maratonajava.javacore.ZZKJunit.servico;
 
 import academy.devdojo.maratonajava.javacore.ZZKJunit.dominio.Pessoa;
 
-import java.util.Objects;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PessoaServico {
     public boolean eAdulto (Pessoa pessoa){
-        Objects.requireNonNull(pessoa,"Pessoa não pode ser null");
+     //   Objects.requireNonNull(pessoa,"Pessoa não pode ser null");
+        if (pessoa == null)throw new IllegalArgumentException();
         return pessoa.getIdade() >= 18;
+    }
+
+    public List<Pessoa> filtroRemovendoNaoAdult (List<Pessoa> pessoaList){
+        return pessoaList.stream().filter(this::eAdulto).collect(Collectors.toList());
     }
 }
--- src/test/java/academy/devdojo/maratonajava/javacore/ZZKJunit/servico/PessoaServicoTest.java
+++ src/test/java/academy/devdojo/maratonajava/javacore/ZZKJunit/servico/PessoaServicoTest.java
@@ -1,18 +1,53 @@
 package academy.devdojo.maratonajava.javacore.ZZKJunit.servico;
 
 import academy.devdojo.maratonajava.javacore.ZZKJunit.dominio.Pessoa;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PessoaServicoTest {
 
+    private Pessoa adulto;
+    private Pessoa naoAdulto;
+    private PessoaServico pessoaServico;
+
+    @BeforeEach
+    public void setUp(){
+        adulto = new Pessoa(18);
+        naoAdulto = new Pessoa(15);
+        pessoaServico = new PessoaServico();
+    }
+
     @Test
     @DisplayName("Uma pessoa não deve ser considerada adulta quando a idade for inferior a 18 anos")
     void eAdulto_ReturnFalse_QuandoAIdadeEMenorQue18() {
-        Pessoa pessoa = new Pessoa(15);
-        PessoaServico pessoaServico = new PessoaServico();
-        assertFalse(pessoaServico.eAdulto(pessoa));
+        assertFalse(pessoaServico.eAdulto(naoAdulto));
+    }
+
+    @Test
+    @DisplayName("Uma pessoa deve ser considerada adulta quando a idade for superior ou igual a 18 anos")
+    void eAdulto_ReturnTrue_QuandoAIdadeEMaiorOuIgualQue18() {
+        assertTrue(pessoaServico.eAdulto(adulto));
+    }
+
+    @Test
+    @DisplayName("Deve lançar NullPointerException com mensagem quando a pessoa for nula")
+    void eAdulto_ThrowException_QuandoAPessoaNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> pessoaServico.eAdulto(null), "Pessoa não pode ser null");
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma lista com apenas adultos")
+    void filtroRemovendoNaoAdult_ReturnListaComApenasAdultos_QuandoAListaPessoaComAdultosEPassada() {
+        Pessoa pessoa1 = new Pessoa(17);
+        Pessoa pessoa2 = new Pessoa(18);
+        Pessoa pessoa3 = new Pessoa(21);
+        List<Pessoa> listPessoas = List.of(pessoa1, pessoa2, pessoa3);
+        assertEquals(2, pessoaServico.filtroRemovendoNaoAdult(listPessoas).size());
     }
 }
